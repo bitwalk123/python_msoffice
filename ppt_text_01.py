e753e3ef300fa55a8b7990a5d5061d040885a066
Fuhito Suguri <bitwalk@gmail.com>
2020-07-11 07:17:21 +0900

    initial

---
+++ ppt_text_01.py
@@ -0,0 +1,13 @@
+# https://qiita.com/code_440/items/22e8539da465686496d3
+from pptx import Presentation
+
+prs = Presentation()
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+
+title.text = "Hello, World!"
+subtitle.text = "python-pptx was here!"
+
+prs.save('test.pptx')
